562d50b6c76b9bd5743f45177882cc00cddfa51e
baldijm <155030467+baldijm@users.noreply.github.com>
2024-01-02 22:15:31 -0600

    last 02 enero

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,12 +25,15 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
+  try {
   await sql`
   UPDATE invoices
   SET customers_id = ${customerId}, amount = ${amountInCents}, status = ${status}
   `;
-
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice '};
+  }
+  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -45,11 +48,22 @@ export async function createInvoice(formData: FormData) {
       const amountInCents = amount * 100;
       const date = new Date().toISOString().split('T')[0];
 
+      try {
       await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+      } catch (error) {
+        return {
+          message: 'Database Error: Failed to Create Invoice',
+        };
+      }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
